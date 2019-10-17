ab6351b55467cb570ca7720a72fc0576315ae91a
LeonardD93 <leonard77dam@yahoo.it>
2019-10-17 18:21:49 +0200

    Generate Fake answer

--- app/Answer.php
+++ app/Answer.php
@@ -1,20 +1,28 @@
 <?php
-
 namespace App;
-
-use App\Question;
 use Illuminate\Database\Eloquent\Model;
-use App\User;
-
 class Answer extends Model
 {
-    public function question(){
+    public function question()
+    {
         return $this->belongsTo(Question::class);
     }
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
-    public function getBodyHtmlAttribute(){
+    public function getBodyHtmlAttribute()
+    {
         return \Parsedown::instance()->text($this->body);
     }
+
+    public static function boot()
+    {
+        parent::boot();
+        static::created(function ($answer) {
+            $answer->question->increment('answers_count');
+            $answer->question->save();
+        });
+    }
 }
+
--- app/Question.php
+++ app/Question.php
@@ -44,3 +44,7 @@ class Question extends Model
     }
 
 }
+
+
+
+
