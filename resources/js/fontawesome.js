dd369c38e0a107bcb3796426683878cec060df56
LeonardD93 <leonard77dam@yahoo.it>
2019-10-18 10:24:58 +0200

    Fontawesome insert

---
+++ resources/js/fontawesome.js
@@ -0,0 +1,13 @@
+
+import fontawesome from '@fortawesome/fontawesome';
+//import '~@fortawesome/fontawesome-free/scss/solid';
+
+
+import faCaretUp from '@fortawesome/fontawesome-free-solid/faCaretUp';
+import faCaretDown from '@fortawesome/fontawesome-free-solid/faCaretDown';
+import faStar from '@fortawesome/fontawesome-free-solid/faStar';
+import faCheck from '@fortawesome/fontawesome-free-solid/faCheck';
+
+
+
+fontawesome.library.add([faCaretUp, faCaretDown, faCheck, faStar]);
