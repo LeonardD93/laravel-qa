a8d6bc396f012cbdc589ec93b45f6f42ed453caa
LeonardD93 <leonard77dam@yahoo.it>
2019-10-21 17:42:10 +0200

    Securing application from xss

--- app/Question.php
+++ app/Question.php
@@ -38,7 +38,7 @@ class Question extends Model
         return "unanswered";
     }
     public function getBodyHtmlAttribute(){
-        return \Parsedown::instance()->text($this->body);
+        return  clean($this->bodyHtml());
     }
     public function answers(){
         return $this->hasMany(Answer::class);
@@ -63,6 +63,19 @@ class Question extends Model
     public function getFavoritesCountAttribute()
     {
         return $this->favorites->count();
+    }
+
+    public function getExcerptAttribute()
+    {
+        return $this->excerpt(250);
+    }
+    public function excerpt($length)
+    {
+        return Str::limit(strip_tags($this->bodyHtml()), $length);
+    }
+    private function bodyHtml()
+    {
+        return \Parsedown::instance()->text($this->body);
     }
 }
 
