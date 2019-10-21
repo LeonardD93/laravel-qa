868c1e25b6433e019ae63fe9921492dac0fa891a
LeonardD93 <leonard77dam@yahoo.it>
2019-10-21 16:08:18 +0200

    add Dry principle

--- app/Answer.php
+++ app/Answer.php
@@ -3,6 +3,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 class Answer extends Model
 {
+    use VotableTrait;
     protected $fillable=['body','user_id'];
 
     public function question()
@@ -47,18 +48,5 @@ class Answer extends Model
     {
         return $this->id === $this->question->best_answer_id;
     }
-    public function votes()
-    {
-        return $this->morphToMany(User::class, 'votable');
-    }
-    public function upVotes()
-    {
-        return $this->votes()->wherePivot('vote', 1);
-    }
-    public function downVotes()
-    {
-        return $this->votes()->wherePivot('vote', -1);
-    }
-
 }
 
