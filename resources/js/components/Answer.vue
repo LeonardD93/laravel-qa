6f1255710be632e84efd9bc8d53cfb985c0c55af
LeonardD93 <leonard77dam@yahoo.it>
2019-10-24 18:24:35 +0200

    Implement event on delete in vue

--- resources/js/components/Answer.vue
+++ resources/js/components/Answer.vue
@@ -78,9 +78,8 @@
 
                             axios.delete(this.endpoint)
                                 .then(res => {
-                                    $(this.$el).fadeOut(500, () => {
-                                        this.$toast.success(res.data.message, "Sucess", { timeout: 3000 });
-                                    })
+                                    this.$emit('deleted')
+
                                 });
                             instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
                         }, true],
