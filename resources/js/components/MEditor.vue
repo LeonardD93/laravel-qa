392d448a155e8949825fa4fc60532c9442ef8e5f
LeonardD93 <leonard77dam@yahoo.it>
2019-10-25 17:12:47 +0200

    make the preview tab functional

--- resources/js/components/MEditor.vue
+++ resources/js/components/MEditor.vue
@@ -3,23 +3,30 @@
         <div class="card-header">
             <ul class="nav nav-tabs card-header-tabs"  role="tablist">
                 <li class="nav-item">
-                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#write" role="tab" aria-controls="home" aria-selected="true">Home</a>
+                    <a class="nav-link active" id="write-tab" data-toggle="tab" href="#write" role="tab" aria-controls="write" aria-selected="true">Write</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#preview" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
+                    <a class="nav-link" id="preview-tab" data-toggle="tab" href="#preview" role="tab" aria-controls="preview" aria-selected="false">Preview</a>
                 </li>
             </ul>
         </div>
         <div class="tab-content" id="myTabContent">
-            <div class="tab-pane fade show active" id="write" role="tabpanel" aria-labelledby="home-tab">  <slot></slot> </div>
-            <div class="tab-pane fade" id="preview" role="tabpanel" aria-labelledby="profile-tab">Preview...</div>
+            <div class="tab-pane fade show active" id="write" role="tabpanel" aria-labelledby="write-tab">  <slot></slot> </div>
+            <div class="tab-pane fade" v-html="preview" id="preview" role="tabpanel" aria-labelledby="preview-tab"></div>
         </div>
     </div>
 </template>
 
 <script>
+    import MarkdownIt from 'markdown-it';
+    const md=new MarkdownIt();
     export default {
-       props:['body']
+       props:['body'],
+        computed:{
+           preview(){
+               return md.render(this.body);
+           }
+        }
     }
 </script>
 
